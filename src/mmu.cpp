b2d2b5bac1e85fc6208833c6eeac11c53ffec0f9
Arthur Giroux <arthurgiroux@gmail.com>
2018-04-07 18:17:06 +0200

    Removed wrong assert and added zero-filling for memory.

--- src/mmu.cpp
+++ src/mmu.cpp
@@ -3,11 +3,11 @@
 #include "mmu.hpp"
 
 MMU::MMU() {
+	memset(memory, 0, sizeof(memory));
 	memcpy(memory, bios, sizeof(bios));
 }
 
 byte MMU::read(const uint16_t& addr) {
-	assert(addr > 0);
 	assert(addr < (TOTAL_MEMORY - 1));
 	return memory[addr];
 }
@@ -17,13 +17,12 @@ uint16_t MMU::readWord(const uint16_t& addr) {
 }
 
 void MMU::write(const uint16_t& addr, const byte& value) {
-	assert(addr > 0);
 	assert(addr < (TOTAL_MEMORY - 1));
 	memory[addr] = value;
 }
 
 void MMU::writeWord(const uint16_t& addr, const uint16_t& value) {
-    assert(addr < (TOTAL_MEMORY - 1));
+	assert(addr < (TOTAL_MEMORY - 2));
 	memory[addr] = value & 0x00FF;
 	memory[addr+1] = value >> 8;
 }
