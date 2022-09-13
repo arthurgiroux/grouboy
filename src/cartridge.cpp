9a9c30f6b7d6a7da5f7101ca8290ccf83f7734fc
Arthur Giroux <arthurgiroux@gmail.com>
2022-09-13 22:23:44 +0200

    Reading cartridge type from header.

--- src/cartridge.cpp
+++ src/cartridge.cpp
@@ -1,8 +1,8 @@
 #include "cartridge.hpp"
 
+#include <algorithm>
 #include <cstring>
 #include <utility>
-#include <algorithm>
 
 Cartridge::Cartridge(std::vector<byte> binaryData) : data(std::move(binaryData))
 {
@@ -28,6 +28,7 @@ void Cartridge::readHeader()
 {
 	readTitle();
 	readManufacturerCode();
+	readType();
 }
 
 void Cartridge::readTitle()
@@ -42,4 +43,79 @@ void Cartridge::readManufacturerCode()
 {
 	const byte* startAddr = data.data() + MANUFACTURER_CODE_START_ADDR;
 	manufacturerCode = std::string(startAddr, startAddr + MANUFACTURER_CODE_LENGTH);
+}
+
+std::string Cartridge::cartridgeTypeToString(CartridgeType value)
+{
+	switch (value)
+	{
+	case 0x00:
+		return "ROM_ONLY";
+	case 0x01:
+		return "MBC1";
+	case 0x02:
+		return "MBC1_RAM";
+	case 0x03:
+		return "MBC1_RAM_BATTERY";
+	case 0x05:
+		return "MBC2";
+	case 0x06:
+		return "MBC2_BATTERY";
+	case 0x08:
+		return "ROM_RAM_1";
+	case 0x09:
+		return "ROM_RAM_BATTERY_1";
+	case 0x0B:
+		return "MMM01";
+	case 0x0C:
+		return "MMM01_RAM";
+	case 0x0D:
+		return "MMM01_RAM_BATTERY";
+	case 0x0F:
+		return "MBC3_TIMER_BATTERY";
+	case 0x10:
+		return "MBC3_TIMER_RAM_BATTERY_2";
+	case 0x11:
+		return "MBC3";
+	case 0x12:
+		return "MBC3_RAM_2";
+	case 0x13:
+		return "MBC3_RAM_BATTERY_2";
+	case 0x19:
+		return "MBC5";
+	case 0x1A:
+		return "MBC5_RAM";
+	case 0x1B:
+		return "MBC5_RAM_BATTERY";
+	case 0x1C:
+		return "MBC5_RUMBLE";
+	case 0x1D:
+		return "MBC5_RUMBLE_RAM";
+	case 0x1E:
+		return "MBC5_RUMBLE_RAM_BATTERY";
+	case 0x20:
+		return "MBC6";
+	case 0x22:
+		return "MBC7_SENSOR_RUMBLE_RAM_BATTERY";
+	case 0xFC:
+		return "POCKET_CAMERA";
+	case 0xFD:
+		return "BANDAI_TAMA5";
+	case 0xFE:
+		return "HUC3";
+	case 0xFF:
+		return "HUC1_RAM_BATTERY";
+	default:
+		return "UNKNOWN";
+	}
+}
+
+Cartridge::CartridgeType Cartridge::getType() const
+{
+	return type;
+}
+
+void Cartridge::readType()
+{
+	type = static_cast<CartridgeType>(data[CARTRIDGE_TYPE_ADDR]);
 }
--- src/cartridge.hpp
+++ src/cartridge.hpp
@@ -38,6 +38,50 @@ class Cartridge
 	 */
 	std::vector<byte>& getData();
 
+	/**
+	 * Represents the type of hardware presents on the cartridge
+	 */
+	enum CartridgeType
+	{
+		UNKNOWN = -1,
+		ROM_ONLY = 0x00,
+		MBC1 = 0x01,
+		MBC1_RAM = 0x02,
+		MBC1_RAM_BATTERY = 0x03,
+		MBC2 = 0x05,
+		MBC2_BATTERY = 0x06,
+		ROM_RAM_1 = 0x08,
+		ROM_RAM_BATTERY_1 = 0x09,
+		MMM01 = 0x0B,
+		MMM01_RAM = 0x0C,
+		MMM01_RAM_BATTERY = 0x0D,
+		MBC3_TIMER_BATTERY = 0x0F,
+		MBC3_TIMER_RAM_BATTERY_2 = 0x10,
+		MBC3 = 0x11,
+		MBC3_RAM_2 = 0x12,
+		MBC3_RAM_BATTERY_2 = 0x13,
+		MBC5 = 0x19,
+		MBC5_RAM = 0x1A,
+		MBC5_RAM_BATTERY = 0x1B,
+		MBC5_RUMBLE = 0x1C,
+		MBC5_RUMBLE_RAM = 0x1D,
+		MBC5_RUMBLE_RAM_BATTERY = 0x1E,
+		MBC6 = 0x20,
+		MBC7_SENSOR_RUMBLE_RAM_BATTERY = 0x22,
+		POCKET_CAMERA = 0xFC,
+		BANDAI_TAMA5 = 0xFD,
+		HUC3 = 0xFE,
+		HUC1_RAM_BATTERY = 0xFF
+	};
+
+	static std::string cartridgeTypeToString(CartridgeType value);
+
+	/**
+	 * Get the hardware type of the cartridge
+	 * @return the type retrieved from the cartridge
+	 */
+	CartridgeType getType() const;
+
   private:
 	/**
 	 * Read the cartridge header from the binary data
@@ -54,6 +98,11 @@ class Cartridge
 	 * Read the manufacturer code from the header.
 	 */
 	void readManufacturerCode();
+
+	/**
+	 * Read the hardware type from the header.
+	 */
+	void readType();
 
 	/**
 	 * The binary data of the cartridge (ROM/RAM etc.)
@@ -89,5 +138,15 @@ class Cartridge
 	 * The start address of the manufacturer code in the binary data
 	 */
 	static const int MANUFACTURER_CODE_START_ADDR = 0x013F;
+
+	/**
+	 * The address of the cartridge type in the binary data
+	 */
+	static const int CARTRIDGE_TYPE_ADDR = 0x0147;
+
+	/**
+	 * The cartridge type read from the header
+	 */
+	CartridgeType type = CartridgeType::UNKNOWN;
 };
 #endif // GBEMULATOR_CARTRIDGE_HPP
