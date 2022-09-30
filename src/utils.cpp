4aaed3378550d2e926d56a3b52640047faf6ae64
Arthur Giroux <arthurgiroux@gmail.com>
2022-09-30 21:29:42 +0200

    Moved utils implementation to .cpp and added utils for bit testing.

---
+++ src/utils.cpp
@@ -0,0 +1,41 @@
+#include "utils.hpp"
+
+namespace utils
+{
+
+uint16_t createAddrFromHighAndLowBytes(byte msb, byte lsb)
+{
+	return (msb << 8u) | lsb;
+}
+
+byte getMsbFromWord(uint16_t value)
+{
+	return value >> 8;
+}
+
+byte getLsbFromWord(uint16_t value)
+{
+	return static_cast<byte>(value);
+}
+
+void increment16BitsValueStoredIn8BitsValues(byte& msb, byte& lsb)
+{
+	uint16_t value = (msb << 8) | lsb;
+	value++;
+	msb = (value >> 8);
+	lsb = value & 0xFF;
+}
+
+void decrement16BitsValueStoredIn8BitsValues(byte& msb, byte& lsb)
+{
+	uint16_t value = (msb << 8) | lsb;
+	value--;
+	msb = (value >> 8);
+	lsb = value & 0xFF;
+}
+
+bool isNthBitSet(int value, int bitPosition)
+{
+    return (value & (1 << bitPosition)) > 0;
+}
+} // namespace utils
--- tests/test_utils.cpp
+++ tests/test_utils.cpp
@@ -3,20 +3,22 @@
 
 using namespace utils;
 
-void assertIncrement16BitsValueStoredIn8BitsValues(uint16_t value, uint16_t expectedValue) {
+void assertIncrement16BitsValueStoredIn8BitsValues(uint16_t value, uint16_t expectedValue)
+{
 	byte msb = getMsbFromWord(value);
 	byte lsb = getLsbFromWord(value);
-    increment16BitsValueStoredIn8BitsValues(msb, lsb);
+	increment16BitsValueStoredIn8BitsValues(msb, lsb);
 	ASSERT_EQ(msb, getMsbFromWord(expectedValue));
-    ASSERT_EQ(lsb, getLsbFromWord(expectedValue));
+	ASSERT_EQ(lsb, getLsbFromWord(expectedValue));
 }
 
-void assertDecrement16BitsValueStoredIn8BitsValues(uint16_t value, uint16_t expectedValue) {
-    byte msb = getMsbFromWord(value);
-    byte lsb = getLsbFromWord(value);
-    decrement16BitsValueStoredIn8BitsValues(msb, lsb);
-    ASSERT_EQ(msb, getMsbFromWord(expectedValue));
-    ASSERT_EQ(lsb, getLsbFromWord(expectedValue));
+void assertDecrement16BitsValueStoredIn8BitsValues(uint16_t value, uint16_t expectedValue)
+{
+	byte msb = getMsbFromWord(value);
+	byte lsb = getLsbFromWord(value);
+	decrement16BitsValueStoredIn8BitsValues(msb, lsb);
+	ASSERT_EQ(msb, getMsbFromWord(expectedValue));
+	ASSERT_EQ(lsb, getLsbFromWord(expectedValue));
 }
 
 TEST(UtilsTest, Increment16BitsValueStoredIn8BitsValuesForValue0ShouldGive1)
@@ -26,27 +28,27 @@ TEST(UtilsTest, Increment16BitsValueStoredIn8BitsValuesForValue0ShouldGive1)
 
 TEST(UtilsTest, Increment16BitsValueStoredIn8BitsValuesForValue200ShouldGive201)
 {
-    assertIncrement16BitsValueStoredIn8BitsValues(200, 201);
+	assertIncrement16BitsValueStoredIn8BitsValues(200, 201);
 }
 
 TEST(UtilsTest, Increment16BitsValueStoredIn8BitsValuesForValueMaxShouldGive0)
 {
-    assertIncrement16BitsValueStoredIn8BitsValues(UINT16_MAX, 0);
+	assertIncrement16BitsValueStoredIn8BitsValues(UINT16_MAX, 0);
 }
 
 TEST(UtilsTest, Decrement16BitsValueStoredIn8BitsValuesForValue0ShouldGiveMax)
 {
-    assertDecrement16BitsValueStoredIn8BitsValues(0, UINT16_MAX);
+	assertDecrement16BitsValueStoredIn8BitsValues(0, UINT16_MAX);
 }
 
 TEST(UtilsTest, Decrement16BitsValueStoredIn8BitsValuesForValue200ShouldGive199)
 {
-    assertDecrement16BitsValueStoredIn8BitsValues(200, 199);
+	assertDecrement16BitsValueStoredIn8BitsValues(200, 199);
 }
 
 TEST(UtilsTest, Decrement16BitsValueStoredIn8BitsValuesForValueMaxShouldGiveMaxMinus1)
 {
-    assertDecrement16BitsValueStoredIn8BitsValues(UINT16_MAX, UINT16_MAX - 1);
+	assertDecrement16BitsValueStoredIn8BitsValues(UINT16_MAX, UINT16_MAX - 1);
 }
 
 TEST(UtilsTest, GetMsbFromWordShouldGiveMsbValue)
@@ -57,14 +59,54 @@ TEST(UtilsTest, GetMsbFromWordShouldGiveMsbValue)
 
 TEST(UtilsTest, GetLsbFromWordShouldGiveLsbValue)
 {
-    uint16_t value = 0x1234;
-    ASSERT_EQ(getLsbFromWord(value), 0x34);
+	uint16_t value = 0x1234;
+	ASSERT_EQ(getLsbFromWord(value), 0x34);
 }
 
 TEST(UtilsTest, CreateAddrFromHighAndLowBytesShouldPutBytesInCorrectPlace)
 {
 	byte msb = 0x12;
 	byte lsb = 0x34;
-    uint16_t value = 0x1234;
-    ASSERT_EQ(createAddrFromHighAndLowBytes(msb, lsb), value);
+	uint16_t value = 0x1234;
+	ASSERT_EQ(createAddrFromHighAndLowBytes(msb, lsb), value);
+}
+
+TEST(UtilsTest, TestBitForValue0ShouldReturnFalseForAllBits)
+{
+	for (int i = 0; i < 16; ++i)
+	{
+		ASSERT_FALSE(utils::isNthBitSet(0, i));
+	}
+}
+
+TEST(UtilsTest, TestBitForPowerOfTwoShouldReturnTrue)
+{
+	int value = 1;
+	for (int i = 0; i < 16; ++i)
+	{
+		ASSERT_TRUE(utils::isNthBitSet(value, i));
+		// Setting the bit by going to the next power of 2.
+		value *= 2;
+	}
+}
+
+TEST(UtilsTest, TestBitForGivenValueShouldGivesExpectedResult)
+{
+	int value = 0b1011010001011010;
+	ASSERT_EQ(utils::isNthBitSet(value, 0), false);
+	ASSERT_EQ(utils::isNthBitSet(value, 1), true);
+	ASSERT_EQ(utils::isNthBitSet(value, 2), false);
+	ASSERT_EQ(utils::isNthBitSet(value, 3), true);
+	ASSERT_EQ(utils::isNthBitSet(value, 4), true);
+	ASSERT_EQ(utils::isNthBitSet(value, 5), false);
+	ASSERT_EQ(utils::isNthBitSet(value, 6), true);
+	ASSERT_EQ(utils::isNthBitSet(value, 7), false);
+	ASSERT_EQ(utils::isNthBitSet(value, 8), false);
+	ASSERT_EQ(utils::isNthBitSet(value, 9), false);
+	ASSERT_EQ(utils::isNthBitSet(value, 10), true);
+	ASSERT_EQ(utils::isNthBitSet(value, 11), false);
+	ASSERT_EQ(utils::isNthBitSet(value, 12), true);
+	ASSERT_EQ(utils::isNthBitSet(value, 13), true);
+	ASSERT_EQ(utils::isNthBitSet(value, 14), false);
+	ASSERT_EQ(utils::isNthBitSet(value, 15), true);
 }
