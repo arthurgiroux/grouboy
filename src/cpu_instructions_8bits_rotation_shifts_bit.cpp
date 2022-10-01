bd0f44ac08adaff5da30744d09ce593409fc1ee7
Arthur Giroux <arthurgiroux@gmail.com>
2022-10-01 17:10:48 +0200

    Refactored bit test to use utils method.

--- src/cpu_instructions_8bits_rotation_shifts_bit.cpp
+++ src/cpu_instructions_8bits_rotation_shifts_bit.cpp
@@ -192,7 +192,7 @@ void CPU::isBitSetForValue(byte value, byte bitPosition)
 	unsetFlag(CpuFlags::SUBSTRACTION);
 	setFlag(CpuFlags::HALF_CARRY);
 	// If the value of the bit is not set then we set the flag zero
-	setFlagIfTrue(((value >> bitPosition) ^ 0x01), CpuFlags::ZERO);
+	setFlagIfTrue(!utils::isNthBitSet(value, bitPosition), CpuFlags::ZERO);
 	lastInstructionTicks = 2;
 }
 
