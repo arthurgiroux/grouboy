1a3a0a1fa8dc974dc44a1014bf596123545f665c
Arthur Giroux <arthurgiroux@gmail.com>
2022-10-30 16:50:36 +0100

    Commented emulator class.

--- src/emulator.hpp
+++ src/emulator.hpp
@@ -6,34 +6,71 @@
 #include "mmu.hpp"
 #include "ppu.hpp"
 
+/**
+ * Emulator class that wraps up all the different components of the system.
+ * This class is in charge of decoding instruction from the MMU and executing them,
+ * then updating the PPU in sync with the other components.
+ */
 class Emulator
 {
   public:
+	/**
+	 * Create a new emulator.
+	 */
 	Emulator();
+
+	/**
+	 * Destroy the emulator.
+	 */
 	~Emulator();
+
+	/**
+	 * Execute the next instruction of the emulator and update the PPU.
+	 */
 	void exec();
+
+	/**
+	 * Resets all the components of the emulator.
+	 */
 	void reset();
 
+	/**
+	 * @return the Picture Processing Unit of the emulator.
+	 */
 	PPU& getPPU()
 	{
 		return ppu;
 	}
 
+	/**
+	 * @return the Memory Management Unit of the emulator.
+	 */
 	MMU& getMMU()
 	{
 		return mmu;
 	}
 
+	/**
+	 * @return the Central Computing Unit of the emulator.
+	 */
 	CPU& getCPU()
 	{
 		return cpu;
 	}
 
+	/**
+	 * @return the input controller used by the emulator.
+	 */
 	InputController& getInputController()
 	{
 		return inputController;
 	}
 
+	/**
+	 * Get the number of CPU ticks elapsed since the start of the emulator.
+	 *
+	 * @return 	the number of ticks
+	 */
 	int getCurrentTicks() const
 	{
 		return currentTicks;
