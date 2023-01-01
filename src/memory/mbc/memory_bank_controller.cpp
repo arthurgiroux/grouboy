aa34e8a33a971ac8c0ecc383cbb893894a75f0b2
Arthur Giroux <arthurgiroux@gmail.com>
2023-01-01 22:15:38 +0100

    WIP: First naive implementation for MBC1.

--- src/memory/mbc/memory_bank_controller.cpp
+++ src/memory/mbc/memory_bank_controller.cpp
@@ -1,4 +1,5 @@
 #include "memory_bank_controller.hpp"
+#include "memory/mbc/mbc1.hpp"
 #include "memory/mbc/mbc_rom_only.hpp"
 
 MemoryBankController::MemoryBankController(Cartridge* cartridge) : _cartridge(cartridge){};
@@ -11,6 +12,10 @@ MemoryBankController::createMemoryBankControllerFromCartridge(Cartridge* cartrid
     {
     case Cartridge::CartridgeType::ROM_ONLY:
         return std::make_unique<MBCRomOnly>(cartridge);
+    case Cartridge::CartridgeType::MBC1:
+    case Cartridge::CartridgeType::MBC1_RAM:
+    case Cartridge::CartridgeType::MBC1_RAM_BATTERY:
+        return std::make_unique<MBC1>(cartridge);
     default:
         return nullptr;
     }
