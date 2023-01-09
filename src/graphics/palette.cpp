59eb8af244bcaa6ee4920f0df2d320425e12b47f
Arthur Giroux <arthurgiroux@gmail.com>
2023-01-09 22:36:48 +0100

    Added Palette first implementation.

---
+++ src/graphics/palette.cpp
@@ -0,0 +1,21 @@
+#include "palette.hpp"
+Palette::Palette(MMU& mmu, word paletteAddr) : _mmu(mmu), _paletteAddr(paletteAddr)
+{
+}
+
+byte Palette::convertColorId(byte colorId)
+{
+    int paletteData = _mmu.read(_paletteAddr);
+
+    /*
+     * The mapping with color is the following:
+     *  Bit 7-6 - Color for index 3
+     *  Bit 5-4 - Color for index 2
+     *  Bit 3-2 - Color for index 1
+     *  Bit 1-0 - Color for index 0
+     */
+    
+    int bitsPerMapping = 2;
+    int bitMask = 0x03;
+    return (paletteData >> (colorId * bitsPerMapping)) & bitMask;
+}
