9ce8b17a4428260608a1fcdb0f8737731c2500e8
Arthur Giroux <arthurgiroux@gmail.com>
2023-01-17 22:57:11 +0100

    Started cleaning up MBC implementation.

--- src/memory/mbc/mbc_rom_only.hpp
+++ src/memory/mbc/mbc_rom_only.hpp
@@ -4,10 +4,22 @@
 #include "memory/mbc/memory_bank_controller.hpp"
 #include <array>
 
+/**
+ * A simple MBC implementation where there's only 1 non-swappable ROM available.
+ * Optional RAM can be available.
+ */
 class MBCRomOnly : public MemoryBankController
 {
   public:
+    /**
+     * Default destructor.
+     */
     virtual ~MBCRomOnly() = default;
+    /**
+     * Create a new MBC with 1 fixed ROM and optional RAM.
+     *
+     * @param cartridge The cartridge to use to retrieve the data.
+     */
     MBCRomOnly(Cartridge* cartridge);
 
     byte readROM(const word& addr) override;
@@ -16,7 +28,11 @@ class MBCRomOnly : public MemoryBankController
     void writeRAM(const word& addr, const byte& value) override;
 
   private:
-    std::array<byte, 8 * 1024> _ram = {};
+    /**
+     * Optional RAM can be up to 8KiB
+     */
+    static const int RAM_SIZE_IN_KB = 8 * 1024;
+    std::array<byte, RAM_SIZE_IN_KB> _ram = {};
 };
 
 #endif // GBEMULATOR_MBC_ROM_ONLY_HPP
