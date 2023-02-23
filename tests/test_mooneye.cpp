cf6b736e18ae6c21851c8f174dfb654fb8012731
Arthur Giroux <arthurgiroux@gmail.com>
2023-02-23 22:00:57 +0100

    Implemented automatic test for DAA MoonEye test.

---
+++ tests/test_mooneye.cpp
@@ -0,0 +1,62 @@
+#include "emulator.hpp"
+#include <gtest/gtest.h>
+
+class MoonEyeTest : public ::testing::Test
+{
+  protected:
+    void SetUp() override
+    {
+        serialTransferManager = std::make_unique<SerialTransferManager>(&emulator.getMMU());
+    }
+
+    void assertTestForRomArePassing(const std::string& romName)
+    {
+        std::string rom = std::string(DATADIR) + "/roms/mooneye/" + romName;
+        ASSERT_TRUE(emulator.getMMU().loadCartridge(rom));
+        bool infiniteJRDetected = false;
+        while (!infiniteJRDetected)
+        {
+
+            emulator.exec();
+            readSerialOutput();
+
+            infiniteJRDetected = isNextInstructionInfiniteJR();
+        }
+
+        // A successful test will write a fibonacci sequence to the serial port
+        std::vector<byte> successSequence = {3, 5, 8, 13, 21, 34};
+        ASSERT_EQ(successSequence, output);
+    }
+
+  private:
+    void readSerialOutput()
+    {
+        if (serialTransferManager->isTransferRequestedOrInProgress())
+        {
+            byte serialData = serialTransferManager->getTransferData();
+            output.push_back(serialData);
+            serialTransferManager->finalizeTransfer();
+        }
+    }
+
+    bool isNextInstructionInfiniteJR()
+    {
+        int nextInstruction = emulator.getMMU().read(emulator.getCPU().getProgramCounter());
+        if (nextInstruction == standardInstructions::JR_n)
+        {
+            int offset = emulator.getMMU().read(emulator.getCPU().getProgramCounter() + 1);
+            return (static_cast<sbyte>(offset) == -2);
+        }
+
+        return false;
+    }
+
+    Emulator emulator;
+    std::unique_ptr<SerialTransferManager> serialTransferManager;
+    std::vector<byte> output = {};
+};
+
+TEST_F(MoonEyeTest, TestForDaaInstrShouldBeSuccessful)
+{
+    assertTestForRomArePassing("acceptance/instr/daa.gb");
+}
