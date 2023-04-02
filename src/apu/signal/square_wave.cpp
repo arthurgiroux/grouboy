00a664d5094da9f3ef4ef050ecc77e383de4ad9f
Arthur Giroux <arthurgiroux@gmail.com>
2023-04-02 21:28:07 +0200

    Added square wave implementation.

---
+++ src/apu/signal/square_wave.cpp
@@ -0,0 +1,55 @@
+#include "square_wave.hpp"
+
+int SquareWave::getAmplitude() const
+{
+    return _wavePatterns[_dutyPattern][_waveDutyPosition];
+}
+
+SquareWave::SquareWave()
+{
+}
+
+void SquareWave::step(int cycles)
+{
+    _frequencyTimerValue -= cycles;
+    // Timer reached the end
+    if (_frequencyTimerValue <= 0)
+    {
+
+        int value = _frequencyTimerValue;
+        resetFrequencyTimer();
+
+        // If we have remainder we deduct it from the new value
+        if (value < 0)
+        {
+            _frequencyTimerValue -= abs(value);
+        }
+
+        nextWaveValue();
+    }
+}
+
+void SquareWave::nextWaveValue()
+{
+    _waveDutyPosition++;
+    if (_waveDutyPosition >= 7)
+    {
+        _waveDutyPosition = 0;
+    }
+}
+
+void SquareWave::setFrequency(int pattern)
+{
+    _frequency = pattern;
+    resetFrequencyTimer();
+}
+
+void SquareWave::resetFrequencyTimer()
+{
+    _frequencyTimerValue = (2048 - _frequency) * 4;
+}
+
+void SquareWave::setDutyPattern(int index)
+{
+    _dutyPattern = index;
+}
