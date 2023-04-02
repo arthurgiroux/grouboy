c149eeb84ba21f7ee195fa5b8e65ff74dfe009ac
Arthur Giroux <arthurgiroux@gmail.com>
2023-04-02 21:28:07 +0200

    Added square wave implementation.

---
+++ src/apu/signal/square_wave.hpp
@@ -0,0 +1,27 @@
+#ifndef GROUBOY_SQUARE_WAVE_HPP
+#define GROUBOY_SQUARE_WAVE_HPP
+
+#include "memory/mmu.hpp"
+#include <array>
+#include <bitset>
+
+class SquareWave
+{
+  public:
+    explicit SquareWave();
+    void step(int cycles);
+    int getAmplitude() const;
+    void setFrequency(int frequency);
+    void setDutyPattern(int pattern);
+
+  private:
+    void resetFrequencyTimer();
+    static constexpr std::array<std::bitset<8>, 4> _wavePatterns = {0b00000001, 0b00000011, 0b00001111, 0b11111100};
+    int _waveDutyPosition = 0;
+    int _dutyPattern = 0;
+    int _frequencyTimerValue = 0;
+    int _frequency = 0;
+    void nextWaveValue();
+};
+
+#endif // GROUBOY_SQUARE_WAVE_HPP
