67259601a99c386c99a1ba00b27f6f073a41864f
Arthur Giroux <arthurgiroux@gmail.com>
2023-04-23 20:40:46 +0200

    Implemented wavelength sweep for channel 1 and start channel's implementation.

--- src/apu/apu.hpp
+++ src/apu/apu.hpp
@@ -1,14 +1,16 @@
 #ifndef GROUBOY_APU_HPP
 #define GROUBOY_APU_HPP
 
+#include "apu/channels/channel1.hpp"
 #include "memory/mmu.hpp"
+#include "timer/timer.hpp"
 
 class APU
 {
   public:
     using AudioBuffer = std::vector<float>;
 
-    explicit APU(MMU* mmu, int samplingFrequency);
+    explicit APU(MMU* mmu, Timer* timer, int samplingFrequency);
     void step(int cycles);
     const AudioBuffer& getAudioBuffer();
     void resetAudioBuffer();
@@ -18,11 +20,14 @@ class APU
     void addSampleToAudioBuffer();
 
     MMU* _mmu;
+    Timer* _timer;
+    Channel1 _channel1;
     int _samplingFrequency;
     int _numberOfCyclesPerAudioSample;
     int _cycleCounter = 0;
     AudioBuffer _audioBuffer = {};
     float _time = 0.f;
+    int _lastDivValue = 0;
 };
 
 #endif // GROUBOY_APU_HPP
