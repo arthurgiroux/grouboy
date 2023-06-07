e3097f2af914d8cb4c5f1d64e66fba13ac75988b
Arthur Giroux <arthurgiroux@gmail.com>
2023-06-07 23:04:51 +0200

    Added channel 2 implementation.

--- src/apu/channels/channel.hpp
+++ src/apu/channels/channel.hpp
@@ -1,16 +1,23 @@
 #ifndef GROUBOY_CHANNEL_HPP
 #define GROUBOY_CHANNEL_HPP
 
+#include "frame_sequencer.hpp"
+
 class Channel
 {
   public:
+    explicit Channel();
     virtual float getAudioSample() = 0;
     virtual void trigger() = 0;
+    virtual void step(int cycles) = 0;
     bool isEnabled() const;
     virtual ~Channel() = default;
+    void tickCounter();
 
   protected:
+    static const int FRAME_SEQUENCER_FREQ = 512;
     bool _enable = false;
+    FrameSequencer _frameSequencer;
 };
 
 #endif // GROUBOY_CHANNEL_HPP
---
+++ src/apu/channels/channel2.hpp
@@ -0,0 +1,13 @@
+#ifndef GROUBOY_CHANNEL2_HPP
+#define GROUBOY_CHANNEL2_HPP
+
+#include "channel_wave.hpp"
+
+class Channel2 : public ChannelWave
+{
+  public:
+    explicit Channel2();
+    ~Channel2() = default;
+};
+
+#endif // GROUBOY_CHANNEL2_HPP
