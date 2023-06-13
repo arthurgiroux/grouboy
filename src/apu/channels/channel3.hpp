4d13a8c946e7b35cfd06d47c78830b00b4f0cf5f
Arthur Giroux <arthurgiroux@gmail.com>
2023-06-13 22:45:45 +0200

    Added channel 3 to APU.

--- src/apu/channels/channel3.hpp
+++ src/apu/channels/channel3.hpp
@@ -1,10 +1,37 @@
 #ifndef GROUBOY_CHANNEL3_HPP
 #define GROUBOY_CHANNEL3_HPP
 
+#include "apu/signal/wave.hpp"
 #include "channel.hpp"
+#include "length_timer.hpp"
 
-class Channel3 : Channel
+class Channel3 : public Channel
 {
+  public:
+    Channel3();
+    ~Channel3() = default;
+    virtual float getAudioSample() override;
+    void step(int cycles) override;
+    void trigger() override;
+    void setWavelength(int wavelength);
+    int getWavelength();
+    Wave<32>& getWave();
+    void setLengthTimer(int timer);
+    void enableLengthTimer(bool value);
+    bool isLengthTimerEnabled() const;
+    void setVolumeControl(int value);
+    int getVolumeControl() const;
+
+  private:
+    static const int LENGTH_TIMER_FREQ = 256;
+    static const int LENGTH_TIMER_DURATION = 256;
+    void tickLengthTimer();
+    int adjustVolume(int value);
+    Wave<32> _wave;
+    LengthTimer _lengthTimer = LengthTimer(LENGTH_TIMER_DURATION);
+    int _wavelength = 0;
+    bool _lengthTimerEnabled = false;
+    int _volumeCtrl = 0;
 };
 
 #endif // GROUBOY_CHANNEL3_HPP
