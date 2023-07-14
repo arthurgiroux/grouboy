8394cfeda89b433736fc1ade7b1e059fb7e62a7c
Arthur Giroux <arthurgiroux@gmail.com>
2023-07-14 14:54:08 +0200

    Added documentation for square wave.

--- src/apu/signal/square_wave.hpp
+++ src/apu/signal/square_wave.hpp
@@ -4,26 +4,109 @@
 #include <array>
 #include <bitset>
 
+/**
+ * A class to generate a square wave based on 4x predefined wave pattern.
+ * The generated wave will be a digital signal with 0 being the low-value and 1 the high-value.
+ * It's possible to control the frequency of the wave, and which pattern to use.
+ */
 class SquareWave
 {
   public:
+    /**
+     * Create a new square wave generator
+     */
     explicit SquareWave();
+
+    /**
+     * Reset the generator to its initial state.
+     */
     void reset();
+
+    /**
+     * Step the generator by a certain amount of CPU cycles.
+     * Depending on the selected frequency, this might generate a new wave sample.
+     *
+     * @param cycles the number of CPU cycles to step.
+     */
     void step(int cycles);
+
+    /**
+     * Get the amplitude of the
+     * @return either 0 or 1
+     */
     int getAmplitude() const;
+
+    /**
+     * Set the frequency of the wave, in CPU cycles.
+     * @param frequency a value in CPU cycles.
+     */
     void setFrequency(int frequency);
+
+    /**
+     * Get the frequency of the wave
+     * @return a value in CPU cycles.
+     */
     int getFrequency() const;
+
+    /**
+     *
+     * Select which wave is used for the generation.
+     * There's 4 different options with different duty cycles and waveform:
+     *  0: duty-cycle = 12.5%
+     *  1: duty-cycle = 25%
+     *  2: duty-cycle = 50%
+     *  3: duty-cycle = 75%
+     *
+     * @param pattern The pattern to select, value between [0, 3]
+     */
     void setDutyPattern(int pattern);
+
+    /**
+     * Get the id of the selected wave pattern
+     * @return A value between [0, 3]
+     */
     int getDutyPattern() const;
+
+    /**
+     * Reset the internal frequency counter.
+     */
     void resetFrequencyTimer();
+
+    /**
+     * Set the current index inside the wave pattern.
+     * @param index A value between [0, 7]
+     */
     void setDutyPosition(int index);
 
   private:
+    /**
+     * The different pre-defined wave pattern, they will be used as bitfield.
+     */
     static constexpr std::array<int, 4> _wavePatterns = {0b00000001, 0b10000001, 0b10000111, 0b01111110};
+
+    /**
+     * Move the signal to the next wave position.
+     */
     void nextWaveValue();
+
+    /**
+     * Which position of the wave we are currently in
+     */
     int _waveDutyPosition = 0;
+
+    /**
+     * The selected wave pattern.
+     */
     int _dutyPattern = 0;
+
+    /**
+     * The internal frequency counter, when it reaches the selected frequency a new sample is generated.
+     */
     int _frequencyTimerValue = 0;
+
+    /**
+     * The frequency of the wave, in CPU cycles.
+     */
     int _frequency = 0;
 };
 
