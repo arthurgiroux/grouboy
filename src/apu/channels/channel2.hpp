27caf36e7cb7f3741b7490041f9921f5381c57c2
Arthur Giroux <arthurgiroux@gmail.com>
2023-08-04 21:19:35 +0200

    Documented Channel 2.

--- src/apu/channels/channel2.hpp
+++ src/apu/channels/channel2.hpp
@@ -3,14 +3,22 @@
 
 #include "pulse_channel.hpp"
 
+/**
+ * Channel 2 is a pulse audio channel that generates a rectangle waveform.
+ * The volume can be set directly or controlled through a sweep.
+ */
 class Channel2 : public PulseChannel
 {
   public:
+    /**
+     * Create a new channel 2
+     * @param highpassCoeff The coefficient to use for the DAC filter
+     */
     explicit Channel2(float highpassCoeff);
-    ~Channel2() = default;
+    ~Channel2() override = default;
 
   private:
-    void triggerImpl() override {};
+    void triggerImpl() override{};
 };
 
 #endif // GROUBOY_CHANNEL2_HPP
