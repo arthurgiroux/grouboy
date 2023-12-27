a2e31277d100cb4d7a90fce12fbf572585d7137d
Arthur Giroux <arthurgiroux@gmail.com>
2023-12-27 09:00:44 -0700

    Exposed a method to read from a specific bank.

--- src/memory/switchable_memory_bank.cpp
+++ src/memory/switchable_memory_bank.cpp
@@ -13,12 +13,7 @@ void SwitchableMemoryBank<NBR_BANK, MEM_SIZE>::switchBank(unsigned int index)
 template <int NBR_BANK, int MEM_SIZE>
 byte SwitchableMemoryBank<NBR_BANK, MEM_SIZE>::read(word addr) const
 {
-    if (addr < MEM_SIZE)
-    {
-        return memory[currentBankId][addr];
-    }
-
-    return 0;
+    return readFromBank(addr, currentBankId);
 }
 
 template <int NBR_BANK, int MEM_SIZE>
@@ -34,6 +29,17 @@ template <int NBR_BANK, int MEM_SIZE>
 unsigned int SwitchableMemoryBank<NBR_BANK, MEM_SIZE>::getBankId() const
 {
     return currentBankId;
+}
+
+template <int NBR_BANK, int MEM_SIZE>
+byte SwitchableMemoryBank<NBR_BANK, MEM_SIZE>::readFromBank(word addr, unsigned int bankId) const
+{
+    if (addr < MEM_SIZE && bankId < NBR_BANK)
+    {
+        return memory[bankId][addr];
+    }
+
+    return 0;
 }
 
 template class SwitchableMemoryBank<2, 8_KiB>;
