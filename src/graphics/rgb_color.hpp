074c5529888e8c889cbc77b2f53e8cef30d53dee
Arthur Giroux <arthurgiroux@gmail.com>
2023-12-30 15:49:43 -0700

    Added RGB color class to use it for palette.

---
+++ src/graphics/rgb_color.hpp
@@ -0,0 +1,51 @@
+#ifndef GROUBOY_RGB_COLOR_HPP
+#define GROUBOY_RGB_COLOR_HPP
+
+#include "common/utils.hpp"
+
+/**
+ * A class for handling RGB color.
+ * Resolution of each color is 1-byte
+ */
+class RGBColor
+{
+  public:
+    /**
+     * Create a new color from the 3 rgb components
+     * @param r amount of red (0, 255)
+     * @param g amount of green (0, 255)
+     * @param b amount of blue (0, 255)
+     */
+    RGBColor(byte r, byte g, byte b);
+
+    /**
+     * Create a new color from a grayscale value
+     * @param grayscale a grayscale value (0, 255)
+     */
+    explicit RGBColor(byte grayscale);
+
+    /**
+     * Get the amount of red in the color
+     * @return a value between 0 and 255
+     */
+    byte getRed() const;
+
+    /**
+     * Get the amount of green in the color
+     * @return a value between 0 and 255
+     */
+    byte getGreen() const;
+
+    /**
+     * Get the amount of blue in the color
+     * @return a value between 0 and 255
+     */
+    byte getBlue() const;
+
+  private:
+    byte red = 0;
+    byte green = 0;
+    byte blue = 0;
+};
+
+#endif // GROUBOY_RGB_COLOR_HPP
