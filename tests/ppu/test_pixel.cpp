acd83949290529c7a56bb281a1c8f65acb6efc0d
Arthur Giroux <arthurgiroux@gmail.com>
2024-01-17 16:18:09 -0700

    Fixed pixel test.

--- tests/ppu/test_pixel.cpp
+++ tests/ppu/test_pixel.cpp
@@ -1,26 +1,28 @@
+#include "graphics/generic_palette.hpp"
 #include "graphics/pixel.hpp"
 #include <gtest/gtest.h>
 
 TEST(PixelTest, PixelColorCanBeSetAndGetCorrectly)
 {
-    auto pixel = Pixel(0x0F, 0, 0, 0);
+    auto pixel = Pixel(0x0F, nullptr, 0, 0);
     ASSERT_EQ(pixel.getColorId(), 0x0F);
 }
 
 TEST(PixelTest, PixelPaletteCanBeSetAndGetCorrectly)
 {
-    auto pixel = Pixel(0, 0x0F, 0, 0);
-    ASSERT_EQ(pixel.getPalette(), 0x0F);
+    GenericPalette palette({RGBColor::BLACK, RGBColor::BLACK, RGBColor::BLACK, RGBColor::BLACK});
+    auto pixel = Pixel(0, &palette, 0, 0);
+    ASSERT_EQ(pixel.getPalette(), &palette);
 }
 
 TEST(PixelTest, PixelSpritePriorityCanBeSetAndGetCorrectly)
 {
-    auto pixel = Pixel(0, 0, 0x0F, 0);
+    auto pixel = Pixel(0, nullptr, 0x0F, 0);
     ASSERT_EQ(pixel.getSpritePriority(), 0x0F);
 }
 
 TEST(PixelTest, PixelBackgroundPriorityCanBeSetAndGetCorrectly)
 {
-    auto pixel = Pixel(0, 0, 0, 0x0F);
+    auto pixel = Pixel(0, nullptr, 0, 0x0F);
     ASSERT_EQ(pixel.getBackgroundPriority(), 0x0F);
 }
