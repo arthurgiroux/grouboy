da9b6754dda83350fe61a045ffa44623f2811847
Arthur Giroux <arthurgiroux@gmail.com>
2024-04-26 16:39:09 -0600

    WASM support and first implementation of HTML frontend.

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,11 @@
-#ifdef __EMSCRIPTEN__
-#include <emscripten/emscripten.h>
-#endif
-
 #include "emulator.hpp"
 #include "gui/emulator_sdl_gui.hpp"
 #include <iostream>
-#include <mutex>
-
-#ifdef __EMSCRIPTEN__
-void emscriptenLoop(void* fct)
-{
-    EmulatorSDLGUI* gui = reinterpret_cast<EmulatorSDLGUI*>(fct);
-    if (gui->shouldQuit())
-    {
-        emscripten_cancel_main_loop();
-        gui->destroy();
-    }
-    else
-    {
-        std::cout << "loop" << std::endl;
-        gui->mainLoop();
-    }
-}
-#endif
 
 int main(int argc, char* args[])
 {
     const std::string file = args[1];
     Emulator emulator;
-    std::cout << "Loading " << file << std::endl;
     if (!emulator.getMMU().loadCartridge(file))
     {
         std::cout << "Couldn't load rom file." << std::endl;
@@ -37,26 +14,18 @@ int main(int argc, char* args[])
 
     EmulatorSDLGUI gui(emulator);
 
-#ifdef __EMSCRIPTEN__
-    gui.enableAudio(false);
-#endif
-
     if (!gui.create())
     {
         std::cerr << "Error while creating GUI." << std::endl;
         return EXIT_FAILURE;
     }
 
-#ifdef __EMSCRIPTEN__
-    emscripten_set_main_loop_arg(emscriptenLoop, &gui, 0, 1);
-#else
     while (!gui.shouldQuit())
     {
         gui.mainLoop();
     }
 
     gui.destroy();
-#endif
 
     return 0;
 }
---
+++ src/wasm/wasm_interface.hpp
@@ -0,0 +1,9 @@
+#ifndef GROUBOY_WASM_INTERFACE_HPP
+#define GROUBOY_WASM_INTERFACE_HPP
+
+extern "C"
+{
+    void startEmulator();
+}
+
+#endif // GROUBOY_WASM_INTERFACE_HPP
