2b7987e9ba27d4fe29be21a9bd4dac178898ccac
Arthur Giroux <arthurgiroux@gmail.com>
2024-05-27 21:38:24 -0600

    Continue dumb implementation of pixel fetcher.

--- src/graphics/background_window_pixel_fetcher.hpp
+++ src/graphics/background_window_pixel_fetcher.hpp
@@ -30,13 +30,25 @@ class BackgroundWindowPixelFetcher
         Push
     };
 
-    void getTile();
+    void stepGetTile();
+    void stepGetTileDataLow();
+    void stepGetTileDataHigh();
+    void pushToFifo();
+    void goToStep(Step step);
 
     PPU* _ppu;
     VRAM* _vram;
     PixelFIFO& _pixelFifo;
     Step _currentStep = Step::GetTile;
     Mode _mode = Mode::BACKGROUND;
+    int _ticksInCurrentStep = 0;
+    int _x = 0;
+    int _y = 0;
+    int _bankId = 0;
+    int _tileAddr = 0;
+    int _tileLine = 0;
+    byte _dataHigh = 0;
+    byte _dataLow = 0;
 };
 
 #endif // GROUBOY_BACKGROUND_WINDOW_PIXEL_FETCHER_HPP
