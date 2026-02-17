af2493b832d0d4ae33d6b7b3bf6f79276b75221b
Arthur Giroux <arthurgiroux@gmail.com>
2026-02-16 20:12:19 -0700

    Making sure the wasm artifact is deployed properly.

--- web/grouboy/next.config.mjs
+++ web/grouboy/next.config.mjs
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: "/grouboy",
-    output: "export"
+    output: "export",
+    webpack: (config) => {
+        config.experiments = {
+            ...config.experiments,
+            asyncWebAssembly: true,
+        };
+        config.module.rules.push({
+            test: /\.wasm$/,
+            type: "asset/resource",
+        });
+        return config;
+    },
 };
 
 export default nextConfig;
