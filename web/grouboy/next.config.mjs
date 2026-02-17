ff068e5bde23c84f51ef6d4f087c77ba8db7c053
Arthur Giroux <arthurgiroux@gmail.com>
2026-02-16 21:33:09 -0700

    Removed webpack rules for webassembly that are causing issues on deployed version.

--- web/grouboy/next.config.mjs
+++ web/grouboy/next.config.mjs
@@ -2,17 +2,6 @@
 const nextConfig = {
     basePath: "/grouboy",
     output: "export",
-    webpack: (config) => {
-        config.experiments = {
-            ...config.experiments,
-            asyncWebAssembly: true,
-        };
-        config.module.rules.push({
-            test: /\.wasm$/,
-            type: "asset/resource",
-        });
-        return config;
-    },
 };
 
 export default nextConfig;
